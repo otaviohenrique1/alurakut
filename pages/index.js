276458b10c53bcc3747453ff18cb2ef097aad3ec
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-07-13 18:53:57 -0300

    Commit 4 dos arquivos

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,26 @@
+import React from 'react';
 import styled from 'styled-components'
 import { Box } from '../src/components/Box';
 import { MainGrid } from '../src/components/MainGrid';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
-import { AlurakutMenu, OrkutNostalgicIconSet } from "../src/lib/AlurakutCommons";
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from "../src/lib/AlurakutCommons";
 
 function ProfileSidebar(props) {
   return (
-    <Box>
+    <Box as="aside">
       <img
         src={`https://github.com/${props.githubUser}.png`}
         alt={`${props.githubUser}`}
         style={{ borderRadius: '8px' }}
       />
+      <hr />
+      <p>
+        <a className="boxLink" href={`https://github.com/${props.githubUser}.png`} >
+          @{`${props.githubUser}`}
+        </a>
+      </p>
+      <hr />
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   );
 }
@@ -26,6 +35,12 @@ export default function Home() {
     'marcobrunodev',
     'felipefialho'
   ];
+
+  const [comunidades, setComunidades] = React.useState([{
+    id: '12802378123789378912789789123896123', 
+    title: 'Eu odeio acordar cedo',
+    image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+  }]);
 
   return (
     <>
@@ -45,6 +60,45 @@ export default function Home() {
             Bem vindo(a)
           </h1>
           <OrkutNostalgicIconSet/>
+        </Box>
+        <Box>
+          <h2 className="subTitle">
+            O que você deseja fazer?
+          </h2>
+          <form onSubmit={function handleCriaComunidade(e) {
+            e.preventDefault();
+            const dadosDoForm = new FormData(e.target);
+
+            console.log('Campo: ', dadosDoForm.get('title'));
+            console.log('Campo: ', dadosDoForm.get('image'));
+
+            const comunidade = {
+              id: new Date().toISOString(),
+              title: dadosDoForm.get('title'),
+              image: dadosDoForm.get('image'),
+            }
+            const comunidadesAtualizadas = [...comunidades, comunidade];
+            setComunidades(comunidadesAtualizadas)
+          }}>
+            <div>
+              <input
+                placeholder="Qual vai ser o nome da sua comunidade?"
+                name="title"
+                aria-label="Qual vai ser o nome da sua comunidade?"
+                type="text"
+                />
+            </div>
+            <div>
+              <input
+                placeholder="Coloque uma URL para usarmos de capa"
+                name="image"
+                aria-label="Coloque uma URL para usarmos de capa"
+              />
+            </div>
+            <button>
+              Criar comunidade
+            </button>
+          </form>
         </Box>
       </div>
       <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -1,12 +1,5 @@
 import styled from 'styled-components'
 
-/*
-  export const Box = styled.div`
-    background: #FFFFFF;
-    border-radius: 8px;
-  `;
-*/
-
 export const Box = styled.div`
   background: #FFFFFF;
   border-radius: 8px;
